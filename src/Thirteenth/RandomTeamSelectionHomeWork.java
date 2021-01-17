0209740d44e4e6dcf0d73a06159fe9aa1f040144
akdl911215 <akdl911215@naver.com>
2021-01-17 21:24:20 +0900

    13일차 과제입니다

---
+++ src/Thirteenth/RandomTeamSelectionHomeWork.java
@@ -0,0 +1,16 @@
+package Thirteenth;
+
+
+public class RandomTeamSelectionHomeWork {
+    public static void main(String[] args) {
+        RadomTeamSelection rt = new RadomTeamSelection
+                (17);
+
+        rt.allocArrTeamMemberZeroValue();
+        System.out.println("------------------");
+        rt.allocArrTeamChoice();
+        System.out.println("------------------");
+        rt.printRandomTeamArr();
+        }
+    }
+
